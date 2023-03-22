b9cf4feb37967e510897f88d60fa9838cb134eb5
Quanhcmus <daicalun61@gmail.com>
2023-03-22 18:48:02 +0700

    add file notebook and requirements 6

---
+++ src/deleteDuplicate.py
@@ -0,0 +1,33 @@
+import csv
+
+class deleteDuplicate():
+    
+    def __init__(self,pathFileName:str):
+        self.contents=self.readFile(pathFileName)
+        
+    
+    @staticmethod
+    def readFile(fileName):
+        with open(fileName,'r') as f:
+            contents=f.readlines()
+        return contents
+    
+    def deleteDuplicate(self):
+        rowUnique=[]
+        indexDuplicate=[]
+        for i in range(len(self.contents)):
+            if self.contents[i] in rowUnique:
+                indexDuplicate.append(i)
+            else:
+                rowUnique.append(self.contents[i])
+        for i in range(len(indexDuplicate)):
+            del self.contents[indexDuplicate[i]-i]
+        print(indexDuplicate)
+        return len(indexDuplicate)
+    
+    def writeFile(self):
+        amountRow=self.deleteDuplicate()
+        with open('output/deleteDuplicate.csv','w') as f:
+            for line in self.contents:
+                f.write(line)
+        print(amountRow,'templates have been deleted')
--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
-from deleteColumn import deleteColumn
-import pandas as pd
-Ob=deleteColumn('input/house-prices.csv')
+
+# Test requirement 5
+# from deleteColumn import deleteColumn
+# import pandas as pd
+# Ob=deleteColumn('input/house-prices.csv')
+# Ob.writeFile()
+
+# df=pd.read_csv('output/deleteColumn.csv')
+
+# print(df.shape)
+
+from deleteDuplicate import deleteDuplicate
+
+Ob=deleteDuplicate('input/house-prices.csv')
 Ob.writeFile()
-
-df=pd.read_csv('output/deleteColumn.csv')
-
-print(df.shape)
-
-
-
-
