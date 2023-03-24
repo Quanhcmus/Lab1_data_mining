4ec8a448f88ea5dbb166197eb383aa904fdc621d
Quanhcmus <daicalun61@gmail.com>
2023-03-24 10:56:21 +0700

    add z-score

--- src/nomarlize.py
+++ src/nomarlize.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import numpy as np
 class Nomarlize():
     def __init__(self,pathFileName,type) :
         self.contents=self.readFile(pathFileName)
@@ -24,7 +25,6 @@ class Nomarlize():
             if len(listTest)==amountAttributes-1:
                 countRow=i
                 break
-        print('row count',countRow)
         return countRow
     
     def numericalAttributes(self):
@@ -58,18 +58,48 @@ class Nomarlize():
                 try:
                     value=float(self.contents[i][j])
                     nomarlize=(value-minValue)/(maxValue-minValue)
-                    self.contents[i][j]=str(nomarlize)
+                    self.contents[i][j]='{:.8f}'.format(nomarlize)
+                except ValueError:
+                    pass
+    
+    def zScore(self):
+        listNumericalAttributes=self.numericalAttributes()
+        listNumerical=[]
+        for row in self.contents:
+
+            for j in listNumericalAttributes:
+                try:
+                    value=float(row[j])
+                    listNumerical.append(value)
+                except ValueError:
+                    pass
+        numpyArray=np.array(listNumerical)
+        mean=numpyArray.mean()
+        std=numpyArray.std()
+        for i in range(len(self.contents)):
+            for j in listNumericalAttributes:
+                try:
+                    value=float(self.contents[i][j])
+                    nomarlize=(value-mean)/std
+                    self.contents[i][j]='{:.8f}'.format(nomarlize)
                 except ValueError:
                     pass
     
     def writeFile(self):
         if self.type=='min-max':
             self.minMaxScaler()
-            with open('output/nomarlize.csv','w',newline='') as file:
+            with open('output/nomarlize_min-max.csv','w',newline='') as file:
                     writer=csv.writer(file)
                     for row in self.contents:
                         writer.writerow(row)
             print("Normalized a numeric attribute using min-max")
+        elif self.type=='z-score':
+            self.zScore()
+            with open('output/nomarlize_z-score.csv','w',newline='') as file:
+                    writer=csv.writer(file)
+                    for row in self.contents:
+                        writer.writerow(row)
+            print("Normalized a numeric attribute using z-score")
                     
 instance=Nomarlize(sys.argv[1],sys.argv[2])
 instance.writeFile()
